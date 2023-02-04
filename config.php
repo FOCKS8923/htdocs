326d71e8a9c43b2ca099fffeaa01b0577d36b290
notagentfox <78776376+FOCKS8923@users.noreply.github.com>
2023-02-04 07:50:09 +0530

    1

--- config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'cs_user_db');
-?>
--- js/preloader.js
+++
@@ -1,9 +0,0 @@
-var preloader = document.getElementById('preloader');
-
-function fpreloader(){
-  preloader.style.display = 'none';
-}
-setTimeout(function() {
-    var preloader = document.getElementById("preloader");
-    preloader.style.display = "none";
-  }, 3000);
